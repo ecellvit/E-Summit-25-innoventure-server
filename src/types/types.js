1eb8af985d14e405c6c1d9f23c6f77a29e4f0721
DNahar74 <nahardarsh55@gmail.com>
2025-02-20 15:03:52 +0530

    changed package.json start

---
+++ src/types/types.js
@@ -0,0 +1,4 @@
+"use strict";
+//todo: (1) Understand each of these socket.io types
+//todo: (2) Divide the types into different files
+Object.defineProperty(exports, "__esModule", { value: true });
