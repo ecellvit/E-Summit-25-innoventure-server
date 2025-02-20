388661a1db90a76ff0becee5e25b26d0146cea85
DNahar74 <nahardarsh55@gmail.com>
2025-02-20 12:21:19 +0530

    added types and a server

---
+++ src/server/index.ts
@@ -0,0 +1,104 @@
+// //todo: (1) Configure and understand cors settings
+
+// import { createServer } from "node:http";
+// import next from "next";
+// import { Server } from "socket.io";
+
+// //* dev = false means the server is running in production mode
+// const dev = process.env.NODE_ENV !== "production";
+
+// //* hostname refers to the hostname (or domain) where the Next.js server will be available.
+// const hostname = process.env.HOSTNAME || "localhost";
+
+// //* port refers to the port where the Next.js server will be available.
+// const port = 8080;
+
+// // this creates a next server
+// const app = next({ dev, hostname, port });
+// const handler = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   const httpServer = createServer(handler);
+
+//   const io = new Server<
+//     ClientToServerEvents,
+//     ServerToClientEvents,
+//     InterServerEvents,
+//     SocketData
+//   >(httpServer, {
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//     },
+//   });
+
+//   io.on("connection", (socket) => {
+//     // ...
+//   });
+
+//   httpServer
+//     .once("error", (err) => {
+//       console.error(err);
+//       process.exit(1);
+//     })
+//     .listen(port, () => {
+//       console.log(`> Ready on http://${hostname}:${port}`);
+//     });
+// });
+
+import { Server } from "socket.io";
+import http from "http";
+import { ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData } from "../types/types";
+
+const hostname = process.env.HOSTNAME || "localhost";
+const port = 8080;
+const httpServer = http.createServer();
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents
+>( httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("hello", (data) => {
+    const { name, age } = data;
+    if (name == "Darsh" && age == 19) {
+      console.log("Name:", name);
+      console.log("Age:", age);
+      startTimer();
+    } else {
+      console.log("Invalid name or age");
+      socket.emit("noArg");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+function startTimer() {
+  console.log("Starting timer...");
+  const timer = setInterval(() => {
+    const date = new Date();
+    console.log("Date:", date)
+    io.emit("timer", { message: "30 seconds passed", timestamp: date });
+  }, 2000);
+  
+  setTimeout(() => {
+    clearInterval(timer);
+    console.log("Timer stopped after 30 seconds");
+  }, 31000);
+}
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://${hostname}:${port}`);
+});
---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+//todo: (1) Understand each of these socket.io types
+//todo: (2) Divide the types into different files
+
+export interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+  timer: (data: TimerData) => void;
+}
+
+export interface ClientToServerEvents {
+  hello: (data: SocketData) => void;
+}
+
+export interface InterServerEvents {
+  ping: () => void;
+}
+
+export interface SocketData {
+  name: string;
+  age: number;
+}
+
+export interface TimerData {
+  message: string; 
+  timestamp: Date;
+}
